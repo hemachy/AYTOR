c2457931dea8be1a9868f5581bc3af61f517fe9e
hemachy <122871924+hemachy@users.noreply.github.com>
2023-05-29 10:01:34 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(function(){
+// **Banner Slick Slider
+
+    $('#banner').slick({
+     arrows: false,
+     dots:true,
+     autoplay:true,
+     
+    });
+
+
+    $('#bannerSm').slick({
+      arrows: false,
+      dots:false,
+      autoplay:true,
+      
+     });
+
+  });
+      
